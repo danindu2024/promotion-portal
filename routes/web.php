fc0eb2917561a4714d8d44534892107658cd68a1
danindu2024 <daninduransika@gmail.com>
2026-02-18 10:25:57 +0530

    feat: add api for cascading dropdown

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\LocationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Location API (cascading dropdowns)
+Route::prefix('api/locations')->group(function () {
+    Route::get('/provinces', [LocationController::class, 'provinces']);
+    Route::get('/districts', [LocationController::class, 'districts']);
+    Route::get('/ds-divisions', [LocationController::class, 'dsDivisions']);
 });
