b20de3bea6229cd2f8f6fd857f2aa6464803de44
danindu2024 <daninduransika@gmail.com>
2026-02-23 12:41:15 +0530

    feat: add notification dismiss icon

--- resources/js/Pages/Registry/DataEntry.vue
+++ resources/js/Pages/Registry/DataEntry.vue
@@ -42,37 +42,79 @@
                 <div ref="alertArea">
                     <div
                         v-if="successMsg"
-                        class="mb-6 p-4 bg-green-50 text-green-700 border border-green-200 rounded-md flex items-center"
+                        class="mb-6 p-4 bg-green-50 text-green-700 border border-green-200 rounded-md flex items-center justify-between"
                     >
-                        <svg
-                            class="w-5 h-5 mr-2 flex-shrink-0"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
+                        <div class="flex items-center">
+                            <svg
+                                class="w-5 h-5 mr-2 flex-shrink-0"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            {{ successMsg }}
+                        </div>
+                        <button
+                            @click="successMsg = ''"
+                            class="text-green-500 hover:text-green-700 focus:outline-none"
+                            aria-label="Dismiss"
                         >
-                            <path
-                                fill-rule="evenodd"
-                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                                clip-rule="evenodd"
-                            />
-                        </svg>
-                        {{ successMsg }}
+                            <svg
+                                class="w-5 h-5"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M6 18L18 6M6 6l12 12"
+                                ></path>
+                            </svg>
+                        </button>
                     </div>
                     <div
                         v-if="errorMsg"
-                        class="mb-6 p-4 bg-red-50 text-red-700 border border-red-200 rounded-md flex items-center"
+                        class="mb-6 p-4 bg-red-50 text-red-700 border border-red-200 rounded-md flex items-center justify-between"
                     >
-                        <svg
-                            class="w-5 h-5 mr-2 flex-shrink-0"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
+                        <div class="flex items-center">
+                            <svg
+                                class="w-5 h-5 mr-2 flex-shrink-0"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                            {{ errorMsg }}
+                        </div>
+                        <button
+                            @click="errorMsg = ''"
+                            class="text-red-500 hover:text-red-700 focus:outline-none"
+                            aria-label="Dismiss"
                         >
-                            <path
-                                fill-rule="evenodd"
-                                d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-                                clip-rule="evenodd"
-                            />
-                        </svg>
-                        {{ errorMsg }}
+                            <svg
+                                class="w-5 h-5"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M6 18L18 6M6 6l12 12"
+                                ></path>
+                            </svg>
+                        </button>
                     </div>
                 </div>
 
