8a5ec62bb4181e1feccf6e1cc6ccb4ee1657c976
danindu2024 <daninduransika@gmail.com>
2026-03-06 15:32:42 +0530

    feat: add regex pattern to validate ID number

--- app/Services/RegistryValidator.php
+++ app/Services/RegistryValidator.php
@@ -62,7 +62,7 @@ class RegistryValidator
             'contact_number' => ['required', 'string', 'regex:/^0\d{9}$/'],
             'whatsapp_number' => ['nullable', 'string', 'regex:/^0\d{9}$/'],
             'email' => 'nullable|email',
-            'national_id_number' => 'nullable|string|max:12',
+            'national_id_number' => ['nullable', 'string', 'regex:/^(?:[0-9]{9}[vVxX]|[0-9]{12})$/'],
         ];
 
         // Category-Specific Rules
