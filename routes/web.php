aa708b9af67c82741b0f475f7d7d421e06e24ea1
danindu2024 <daninduransika@gmail.com>
2026-03-20 16:04:58 +0530

    feat: add main dashboard url to main route folder

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return redirect('/data-entry');
 });
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Registry/Dashboard');
+})->name('dashboard');
+
 Route::get('/data-entry', function () {
     return Inertia::render('Registry/DataEntry');
 });
