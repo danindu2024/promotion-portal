20bdec910d345fd83e5f65268922b0a3e6be4318
danindu2024 <daninduransika@gmail.com>
2026-03-24 16:10:20 +0530

    feat: added user routers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\UserManagementController;
 
 Route::get('/', function () {
     return redirect('/data-entry');
@@ -18,3 +19,9 @@ Route::get('/data-entry', function () {
 Route::get('/review', function () {
     return Inertia::render('Registry/Review');
 });
+
+Route::get('/admin/users', [UserManagementController::class, 'index'])->name('admin.users.index');
+Route::get('/api/users', [UserManagementController::class, 'index']);
+Route::post('/api/users', [UserManagementController::class, 'store']);
+Route::put('/api/users/{id}', [UserManagementController::class, 'update']);
+Route::delete('/api/users/{id}', [UserManagementController::class, 'destroy']);
