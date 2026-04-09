2525b7620e8d5efea2a8a8ce5259bbb4890d62ad
danindu2024 <daninduransika@gmail.com>
2026-04-09 11:31:04 +0530

    feat: add sheet protection to prevent tab modifications

--- app/Exports/RegistryTemplateExport.php
+++ app/Exports/RegistryTemplateExport.php
@@ -3,8 +3,10 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class RegistryTemplateExport implements WithMultipleSheets
+class RegistryTemplateExport implements WithMultipleSheets, WithEvents
 {
     /**
      * @return array
@@ -15,6 +17,20 @@ class RegistryTemplateExport implements WithMultipleSheets
             new Sheets\SelfEmployedTemplateSheet(),
             new Sheets\TradeTemplateSheet(),
             new Sheets\OptionsSheet(),
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                // Access the spreadsheet parent from the current sheet
+                $spreadsheet = $event->sheet->getDelegate()->getParent();
+                
+                // Protect the workbook structure (prevents renaming, deleting, adding sheets)
+                $spreadsheet->getSecurity()->setLockStructure(true);
+                $spreadsheet->getSecurity()->setWorkbookPassword('registry_template');
+            },
         ];
     }
 }
